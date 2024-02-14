196ae466104e5931abee0bd1e3c71afdfb49ee48
Antoni <antoni138@interia.pl>
2024-02-14 13:35:49 +0100

    created-section

--- src/js/script.js
+++ src/js/script.js
@@ -2,6 +2,7 @@ const menu = document.querySelector('.nav__menu')
 const bars = document.querySelector('.nav__bars')
 const allNavItems = document.querySelectorAll('.nav__menu-link')
 const accordionContent = document.querySelectorAll('.questions__accordion-content')
+const offersContent = document.querySelectorAll('.offers__card')
 
 const showMenu = () => {
 	menu.classList.toggle('nav__menu--active')
